acaf3f4310a510fb321ceb0423055bfce23f3367
sujidev9839 <sujitkumar24079@gmail.com>
2025-06-18 17:02:54 +0000

    tokencomment

--- src/pages/UserInformation.tsx
+++ src/pages/UserInformation.tsx
@@ -211,7 +211,7 @@ function UserInformation() {
         }
         setLoading(true)
         const cardElement = elements.getElement(CardElement);
-        const { token } = await stripe.createToken(cardElement);
+        //const { token } = await stripe.createToken(cardElement);
 
         const result = await stripe.createPaymentMethod({
             type: 'card',
